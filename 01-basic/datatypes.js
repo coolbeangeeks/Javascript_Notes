94e816b4972e69e83c5fd2132c6b38fe80b09e1f
coolbeangeeks <109415685+coolbeangeeks@users.noreply.github.com>
2023-10-02 15:00:25 +0000

    datatypes basic

---
+++ 01-basic/datatypes.js
@@ -0,0 +1,13 @@
+"use strict" // if used in code will treat the newer version of js
+
+let name="abhi"
+let age=18
+
+//  primitive datatypes
+// number => 2 to power 53
+// string
+// boolean => true/false
+// null=> standalone value
+// undefined => not defined
+
+console.log(typeof null)// will give object
